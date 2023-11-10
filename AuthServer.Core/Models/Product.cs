31089d3141a499c32663ad0fdc39c4dc3ba0ecd7
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-11-10 13:30:39 +0300

    Added Entities

---
+++ AuthServer.Core/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Decimal price { get; set; }
+        public int Stock { get; set; }
+        public string UserId{ get; set; }
+    }
+}
---
+++ AuthServer.Core/Models/UserRefreshToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Models
+{
+    public class UserRefreshToken
+    {
+        public string UserId { get; set; }
+        public string Code { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}

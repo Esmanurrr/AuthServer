033f122abf15477a14a1c97fd4f989b825b20c73
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-11-10 14:04:09 +0300

    Added ErrorDto

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<String> Errors { get; private set; } //dont change the value 
+        public bool IsShow { get; private set; }
+        //true - user can see the error, false - developer can see the error
+
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            isShow = true;
+        }
+
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+    }
+}

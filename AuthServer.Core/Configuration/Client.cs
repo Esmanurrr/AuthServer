7b0887687e422f83a3b8e99b343b60fe4d05184b
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-03-06 09:21:56 +0300

    Added ITokenService

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration
+{
+    public class Client//it can be web app or mobile app, just for request to authserver
+    {
+        public string Id { get; set; } //username
+        public string Secret { get; set; } //password
+
+        //ex: www.myapi1.com www.myapi2.com can access
+        public List<string> Audiences { get; set; }//which api can access?
+    }
+}

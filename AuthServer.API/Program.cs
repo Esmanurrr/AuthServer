ad85d2792005a3f92700181ad466ad90847eafd9
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-03-07 11:52:02 +0300

    Added Options pattern

--- AuthServer.API/Program.cs
+++ AuthServer.API/Program.cs
@@ -1,6 +1,10 @@
+using SharedLibrary.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.Configure<CustomTokenOptions>(builder.Configuration.GetSection("TokenOption"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
